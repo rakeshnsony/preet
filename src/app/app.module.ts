283747b71842464912e013b75c4a1917a942a7b3
Rakesh <rakeshnsony@gmail.com>
2018-03-21 20:15:36 +0530

    done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,21 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { PatientComponent } from './patient/patient.component';
+import { ListComponent } from './list/list.component';
+import { SearchComponent } from './list/search/search.component';
+import { RecordsComponent } from './list/records/records.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    PatientComponent,
+    ListComponent,
+    SearchComponent,
+    RecordsComponent
   ],
   imports: [
     BrowserModule
